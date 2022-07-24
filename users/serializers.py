1a549ec279664303663e1857501eb9a8d1fff4d0
YusufAbdul <abdul17yusuf@gmail.com>
2022-07-24 22:44:39 +0100

    commit 02

--- users/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-from django.conf import settings
-from . import models
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.User
-        fields =  "__all__" 
