0a2bb0004bd38b02baf59958bd5f64b9767ce162
YusufAbdul <abdul17yusuf@gmail.com>
2022-07-26 11:21:35 +0100

    Allowed host to any

--- SPRINT_PROJECT/settings.py
+++ SPRINT_PROJECT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(8*upz$@d^=%d@vk^bkt$bf!6*rsi6avsa60k5p#pn+!^zm4$z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
