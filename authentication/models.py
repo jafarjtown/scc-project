f7c439e813cfb613839684221c1cc96322832b57
Jafaru Idris Tesla <57459852+jafarjtown@users.noreply.github.com>
2022-08-11 01:06:03 +0100

    Update account type field

--- authentication/models.py
+++ authentication/models.py
@@ -11,7 +11,7 @@ ACCOUNT_TYPE = (
 class User(AbstractUser):
     date_of_birth = models.DateField(null=True)
     phone_no = models.CharField(max_length=15)
-    account_type = models.CharField(choices=ACCOUNT_TYPE, default=ACCOUNT_TYPE[1], max_length=7)
+    account_type = models.CharField(choices=ACCOUNT_TYPE, default=ACCOUNT_TYPE[1], max_length=10)
     account_id = models.CharField(max_length=50)
     #to know wether account is kitchen or customers
     is_kitchen = models.BooleanField(default=False)
